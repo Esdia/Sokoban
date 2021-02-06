8ae3a3f0e94bfb6190ded8d58b4f1f2403af269c
Esdia <theo.hermitte@protonmail.com>
2021-02-06 23:11:13 +0100

    Added the abstract PriorityQueue class

---
+++ src/esdia/sokoban/sequences/PriorityQueue.java
@@ -0,0 +1,19 @@
+package esdia.sokoban.sequences;
+
+public abstract class PriorityQueue<Type extends Comparable<Type>> {
+    Sequence<Type> s;
+
+    public PriorityQueue() {
+        this.s = new LinkedList<>();
+    }
+
+    public abstract void insert(Type e);
+
+    public Type extrack() {
+        return s.getHead();
+    }
+
+    public boolean isEmpty() {
+        return s.isEmpty();
+    }
+}
