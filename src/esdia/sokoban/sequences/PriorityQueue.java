14d913433afff422aaebf39f2adeb88b272ba693
Esdia <theo.hermitte@protonmail.com>
2021-02-07 18:33:51 +0100

    Removed useless constructor in PriorityQueue

--- src/esdia/sokoban/sequences/PriorityQueue.java
+++ src/esdia/sokoban/sequences/PriorityQueue.java
@@ -3,11 +3,7 @@ package esdia.sokoban.sequences;
 public abstract class PriorityQueue<V, P extends Comparable<P>> {
     Sequence<Couple<V, P>> s;
 
-    public PriorityQueue() {
-        this.s = new LinkedList<>();
-    }
-
-    public abstract void insert(Couple<V, P> e);
+    abstract void insert(Couple<V, P> e);
 
     public void insert(V value, P priority) {
         Couple<V, P> c = new Couple<>(value, priority);
