2fc3add9d12b3de49d89a631ec247dc35d43bda2
Esdia <theo.hermitte@protonmail.com>
2021-02-08 11:44:38 +0100

    Created a Configuration class and moved all the properties things there

--- src/esdia/sokoban/Sokoban.java
+++ src/esdia/sokoban/Sokoban.java
@@ -1,11 +1,12 @@
 package esdia.sokoban;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
+import esdia.sokoban.global.Configuration;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Properties;
 
 public class Sokoban {
     static InputStream get_input_stream(String filename) {
@@ -38,64 +39,13 @@ public class Sokoban {
         return levels;
     }
 
-    static void properties_from_file(InputStream in, Properties properties) {
-        try {
-            properties.load(in);
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    static boolean user_properties_exist(String path) {
-        File prop_file = new File(path);
-
-        return prop_file.isFile();
-    }
-
-    static void create_user_properties(String default_name, String user_path) {
-        InputStream src = Sokoban.class.getResourceAsStream(default_name);
-        Path dest = new File(user_path).toPath();
-
-        try {
-            Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    static Properties load_properties() {
-        String default_prop = "default.cfg";
-        String prop_file = System.getProperty("user.home") + "/.sokoban";
-        InputStream in;
-
-        in = Sokoban.class.getResourceAsStream(default_prop);
-        Properties default_properties = new Properties();
-        properties_from_file(in, default_properties);
-
-        if (!user_properties_exist(prop_file)) {
-            create_user_properties(default_prop, prop_file);
-            return default_properties;
-        }
-
-        Properties properties = new Properties(default_properties);
-        try {
-            in = new FileInputStream(prop_file);
-            properties_from_file(in, properties);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return properties;
-    }
-
     public static void main(String[] args) {
         if (args.length == 0) {
             System.err.println("ERROR : Please specify a file with levels");
             return;
         }
 
-        Properties properties = load_properties();
+        Configuration config = Configuration.instance();
 
         InputStream in = get_input_stream(args[0]);
         if (in == null) {
@@ -109,6 +59,5 @@ public class Sokoban {
             System.out.println();
         }
 
-        System.out.println(properties.getProperty("Sequence"));
     }
 }
---
+++ src/esdia/sokoban/global/Configuration.java
@@ -0,0 +1,100 @@
+package esdia.sokoban.global;
+
+import esdia.sokoban.Sokoban;
+import esdia.sokoban.sequences.LinkedList;
+import esdia.sokoban.sequences.Sequence;
+import esdia.sokoban.sequences.TableSequence;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.NoSuchElementException;
+import java.util.Properties;
+
+public class Configuration {
+    private static Configuration config = null;
+
+    private final Properties properties;
+
+    private static void properties_from_file(InputStream in, Properties properties) {
+        try {
+            properties.load(in);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean user_properties_exist(String path) {
+        File prop_file = new File(path);
+
+        return prop_file.isFile();
+    }
+
+    private static void create_user_properties(String default_name, String user_path) {
+        InputStream src = Sokoban.class.getResourceAsStream(default_name);
+        Path dest = new File(user_path).toPath();
+
+        try {
+            Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Properties load_properties() {
+        String default_prop = "default.cfg";
+        String prop_file = System.getProperty("user.home") + "/.sokoban";
+        InputStream in;
+
+        in = Sokoban.class.getResourceAsStream(default_prop);
+        Properties default_properties = new Properties();
+        properties_from_file(in, default_properties);
+
+        if (!user_properties_exist(prop_file)) {
+            create_user_properties(default_prop, prop_file);
+            return default_properties;
+        }
+
+        Properties properties = new Properties(default_properties);
+        try {
+            in = new FileInputStream(prop_file);
+            properties_from_file(in, properties);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
+    }
+
+    private Configuration() {
+        this.properties = load_properties();
+    }
+
+    public static Configuration instance() {
+        if (config == null) {
+            config = new Configuration();
+        }
+
+        return config;
+    }
+
+    public String get(String key) {
+        return this.properties.getProperty(key);
+    }
+
+    @SuppressWarnings("unused")
+    public String get(String key, String _default) {
+        return this.properties.getProperty(key, _default);
+    }
+
+    @SuppressWarnings("unused")
+    public <Type> Sequence<Type> new_sequence() {
+        return switch (this.get("Sequence")) {
+            case "Table" -> new TableSequence<>();
+            case "LinkedList" -> new LinkedList<>();
+            default -> throw new NoSuchElementException("Invalid sequence type");
+        };
+    }
+}
