ec870be04b84c37dcb0f4e8f2434d1fcd01b207c
Esdia <theo.hermitte@protonmail.com>
2021-02-23 16:27:33 +0100

    Added a way to toggle fullscreen

--- src/esdia/sokoban/ui/MainWindow.java
+++ src/esdia/sokoban/ui/MainWindow.java
@@ -3,19 +3,21 @@ package esdia.sokoban.ui;
 import esdia.sokoban.game.Game;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class MainWindow implements Runnable {
     Game game;
+    JFrame frame;
+    boolean maximized = false;
 
     private MainWindow(Game game) {
         this.game = game;
+        this.frame = new JFrame();
     }
 
     @Override
     public void run() {
-        JFrame frame = new JFrame();
-
         frame.add(new LevelUI(this.game));
 
         frame.setTitle("Sokoban - Level " + this.game.getCurrentLevel().name());
@@ -28,8 +30,24 @@ public class MainWindow implements Runnable {
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
 
+        frame.getRootPane().registerKeyboardAction(
+                actionEvent -> toggleFullscreen(), KeyStroke.getKeyStroke(KeyEvent.VK_F11, 0), JComponent.WHEN_IN_FOCUSED_WINDOW
+        );
+
         frame.setSize(1300, 1000);
         frame.setVisible(true);
+    }
+
+    public void toggleFullscreen() {
+        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice device = env.getDefaultScreenDevice();
+        if (maximized) {
+            device.setFullScreenWindow(null);
+            maximized = false;
+        } else {
+            device.setFullScreenWindow(this.frame);
+            maximized = true;
+        }
     }
 
     public static void start(Game game) {
