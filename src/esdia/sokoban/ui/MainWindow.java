66d3808b7e7878f9b786ea9782d9c75c1d8edd85
Esdia <theo.hermitte@protonmail.com>
2021-03-01 11:52:00 +0100

    Reworked the indentation for better readability

--- src/esdia/sokoban/ui/MainWindow.java
+++ src/esdia/sokoban/ui/MainWindow.java
@@ -23,12 +23,16 @@ public class MainWindow implements Runnable {
         frame.setTitle("Sokoban - Level " + this.game.getCurrentLevel().name());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.getRootPane().registerKeyboardAction(actionEvent -> {
-            if (game.nextLevel()) {
-                frame.setTitle("Sokoban - Level " + this.game.getCurrentLevel().name());
-                frame.repaint();
-            }
-        }, KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
+        frame.getRootPane().registerKeyboardAction(
+                actionEvent -> {
+                    if (game.nextLevel()) {
+                        frame.setTitle("Sokoban - Level " + this.game.getCurrentLevel().name());
+                        frame.repaint();
+                    }
+                },
+                KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0),
+                JComponent.WHEN_IN_FOCUSED_WINDOW
+        );
 
         frame.getRootPane().registerKeyboardAction(
                 actionEvent -> toggleFullscreen(), KeyStroke.getKeyStroke(KeyEvent.VK_F11, 0), JComponent.WHEN_IN_FOCUSED_WINDOW
